4bbace23f90ed4821c3fea05841cc41bb6906c4a
Luigi Finetti <luigifinetti@mi.unc.edu.ar>
2022-10-09 18:36:35 -0300

    PYR-21 Correcciones PEP8

--- endpoints/helloworld.py
+++ endpoints/helloworld.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 async def helloworld():
     return {"message": "Hello World"}
-
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ app = FastAPI()
 app.include_router(helloworld.router)
 
 origins = [
-	'http://localhost:3000'
+    'http://localhost:3000'
 ]
 
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins = origins,
-	allow_credentials = True,
-	allow_methods = ['GET'],
-	allow_headers = ['Content-Type','application/xml']
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET'],
+    allow_headers=['Content-Type', 'application/xml'],
 )
