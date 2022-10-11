aab3f57e3158771c3f3f569c200809904b67137e
Leandro200013 <leandro.lopez@mi.unc.edu.ar>
2022-10-10 21:35:42 -0300

    PYR-44 raise status code agregado

--- endpoints/listar_partidas.py
+++ endpoints/listar_partidas.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from db import *
 import json
 
@@ -6,7 +6,7 @@ router = APIRouter()
 
 
 @router.get("/lista_partidas")
-async def listar_partidas():
+async def listar_partidas(status_code=status.HTTP_200_OK):
     with db_session:
         lista = []
         for p in select(p for p in Partida):
@@ -22,18 +22,6 @@ async def listar_partidas():
             }
             lista.append(partida_datos)
         if lista == []:
-            return (status.HTTP_400_BAD_REQUEST,
-                    json.dumps({'type': 'no se encontraron resultados'}))
-        return (status.HTTP_200_OK, json.dumps(lista))
-
-
-@router.get('/test_lista')
-async def test_lista():
-    with db_session:
-        u1 = Usuario(nombre_usuario='juan', email='juan.lopez@mi.unc.edu.ar',
-                     contraseña='@Leandro013', verificado=True)
-        p1 = Partida(nombre='partida', status='disponible', cant_jugadores=3,
-                     cant_juegos=10, cant_rondas=10, creador=u1)
-        p1.flush()
-        id_partida = p1.partida_id
-        return {'status': '200 ok', 'id_partida': id_partida}
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='No se encontraron partidas')
+        return json.dumps(lista)
