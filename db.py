45621c5c6296f1a88b5369272cce195e88a4bbd4
RooCordoba <roco-fs@hotmail.com>
2022-10-11 23:11:28 -0300

    Creacion de usuario, sin test, arreglo de errores.

--- db.py
+++ db.py
@@ -69,13 +69,14 @@ def email_exist(email: str):
 #  Verifica que el password tengo al menos una mayuscula,
 #  al menos una minuscula y al menos un numero
 def password_is_correct(pas: str):
-    upper=any(c.isupper() for c in pas)
-    lower=any(c.islower() for c in pas)
-    digit=any(c.isdigit() for c in pas)
+    upper = any(c.isupper() for c in pas)
+    lower = any(c.islower() for c in pas)
+    digit = any(c.isdigit() for c in pas)
     return (upper) and (lower) and (digit)
-   
 
 #  Crea al usuario con el nombre, el password y el email introducido.
+
+
 def crear_usuario(name: str, password: str, myemail: str):
     Usuario(
         nombre_usuario=name,
@@ -84,4 +85,3 @@ def crear_usuario(name: str, password: str, myemail: str):
         verificado=True,
         avatar="Default Avatar"
     )
-    
