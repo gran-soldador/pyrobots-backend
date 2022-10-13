00707cc186cdcb52c9d54cb6f05cf23fbe12d66d
RooCordoba <roco-fs@hotmail.com>
2022-10-12 21:24:21 -0300

    Ahora se puede subir o no avatar y los guarda localmente.

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from pony.orm import *
+import sqlite3
 
 db = Database()
 
@@ -55,8 +56,7 @@ def user_exist(username: str):
     if Usuario.get(nombre_usuario=username) is not None:
         return True
     else:
-        return False
-
+        return False 
 
 #  Verifica si el email ya está registrado
 def email_exist(email: str):
@@ -75,13 +75,42 @@ def password_is_correct(pas: str):
     return (upper) and (lower) and (digit)
 
 #  Crea al usuario con el nombre, el password y el email introducido.
-
-
 def crear_usuario(name: str, password: str, myemail: str):
     Usuario(
         nombre_usuario=name,
         contraseña=password,
         email=myemail,
         verificado=True,
-        avatar="Default Avatar"
+        avatar="userUploads/avatars/DefaultAvatar.png"
     )
+
+#  Crea al usuario con el nombre, el password, el email
+#  y el directorio de su imagen de perfil
+def crear_usuario_con_avatar(name: str, password: str, myemail: str):
+    Usuario(
+        nombre_usuario=name,
+        contraseña=password,
+        email=myemail,
+        verificado=True,
+        avatar="userUploads/avatars/"+name+"UserAvatar"
+    )
+
+#  Cambio del avatar del usuario
+def change_avatar_user(name: str):
+    try:
+        sqliteConnection = sqlite3.connect('main.db')
+        cursor = sqliteConnection.cursor()
+        print("Connected to SQLite")
+        avatar_directory = "userUploads/avatars/"+name+"UserAvatar"
+        sqlite_command = " UPDATE Usuario SET avatar=? WHERE nombre_usuario=?"
+        cursor.execute(sqlite_command, (avatar_directory,name))
+        sqliteConnection.commit()
+        cursor.close()    
+
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite table", error)
+    
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("the sqlite connection is closed")
--- endpoints/registrar_usuario.py
+++ endpoints/registrar_usuario.py
@@ -1,32 +1,41 @@
-from fastapi import APIRouter, HTTPException, status
+from email.mime import image
+from fileinput import filename
+from logging import exception
+from fastapi import APIRouter, HTTPException, status, File, UploadFile
 from db import *
+from PIL import Image
+import imghdr
 
 router = APIRouter()
 
-MAX_NICKNAME_SIZE = 25
-MIN_NICKNAME_SIZE = 8
-MIN_PASSWORD_SIZE = 3
+MAX_NICKNAME_SIZE = 32
+MIN_NICKNAME_SIZE = 1
+MIN_PASSWORD_SIZE = 8
 
 
+#  Creacion de usuario con o sin avatar
 @router.post("/user/registro_de_usuario/",
-             status_code=status.HTTP_200_OK,
              tags=["User Methods"],
              name="Registro de Usuarios")
-async def registro_usuario(name: str, password: str, email: str):
+async def registro_usuario(username: str,
+                           password: str,
+                           useremail: str,
+                           userAvatar: UploadFile = File(None)
+                           ):
     with db_session:
-        if len(name) > MAX_NICKNAME_SIZE:
+        if len(username) > MAX_NICKNAME_SIZE:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Username too long.")
-        elif len(name) < MIN_NICKNAME_SIZE:
+        elif len(username) < MIN_NICKNAME_SIZE:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Username too short.")
         elif len(password) < MIN_PASSWORD_SIZE:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Password too Short.")
-        elif user_exist(name):
+        elif user_exist(username):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="User name already exist.")
-        elif email_exist(email):
+        elif email_exist(useremail):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Email already registered.")
         elif (not password_is_correct(password)):
@@ -34,9 +43,42 @@ async def registro_usuario(name: str, password: str, email: str):
                                 detail="Password inválido, el password "
                                        "requiere al menos una mayuscula, una "
                                        "minusucula y un numero.")
-        elif not ("@" in email):
+        elif not ("@" in useremail):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="Email inválido.")
+        
+        if (userAvatar is None):
+            crear_usuario(username, password, useremail)
+            return {"new user created": username}
         else:
-            crear_usuario(name, password, email)
-            return {"new user created": name}
+            if not userAvatar.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp')):
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                    detail="File is not an image.")
+
+            crear_usuario_con_avatar(username, password, useremail)
+            avatar = "UserAvatar"
+            file_location = f"userUploads/avatars/{username+avatar}"
+            change_avatar_user(username)
+            with open(file_location, "wb+") as file_object:
+                file_object.write(userAvatar.file.read())
+            return {"new user with avatar created": username}
+
+
+#  Actualizacion de avatar del usuario
+@router.post("/user/uploadavatar/", tags=["User Methods"], name="Actualizacion de Avatar del Usuario")
+async def upload_user_avatar(username: str, file: UploadFile = File(...)):
+    with db_session:
+        if not user_exist(username):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="User doesn't exist.")
+
+        elif not file.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp')):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                 detail="File is not an image.")
+        else:
+            avatar = "UserAvatar"
+            file_location = f"userUploads/avatars/{username+avatar}"
+            change_avatar_user(username)
+            with open(file_location, "wb+") as file_object:
+                file_object.write(file.file.read())
+            return {"info": f"file '{file.filename}' saved at '{file_location}'"}
