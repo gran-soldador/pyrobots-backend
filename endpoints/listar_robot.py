7266bf1ae45aef59f2b5aa1012d3d402f3d66674
Guest <guest-ilcuuk@lab75.famaf.unc.edu.ar>
2022-10-27 10:47:23 -0300

    PYR-72 Ahora la lista devuelve la url tambien el avatar del robot

--- endpoints/listar_robot.py
+++ endpoints/listar_robot.py
@@ -1,10 +1,6 @@
-from fastapi import APIRouter, HTTPException, status, Depends, File, UploadFile, Response
-from endpoints.validation import image_to_string
+from fastapi import APIRouter, HTTPException, status, Depends
 from db import *
 from .functions_jwt import *
-from io import BytesIO
-from PIL import Image
-from starlette.responses import StreamingResponse
 
 router = APIRouter()
 
@@ -12,30 +8,9 @@ router = APIRouter()
 @router.get("/lista-robots")
 async def listar_robots(user_id: int = Depends(authenticated_user)):
     with db_session:
-        lista = [{'id': r.robot_id, 'nombre': r.nombre}
+        lista = [{'id': r.robot_id, 'nombre': r.nombre, 'avatar': "http://0.0.0.0:9000/" + r.avatar[12:]}
                  for r in Usuario[user_id].robot]
         if lista == []:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='No se encontraron robots')
         return lista
-
-
-#  Me devuelve el string de la imagen
-@router.post("/imagen_funciona")
-async def imagen_funciona():
-    myPath = image_to_string()
-    return myPath
-
-
-# ANDA- SUBE UNA IMAGEN, ME MUESTRA LA MISMA IMAGEN SUBIDA
-
-# @router.post("/vector_image")
-# def image_filter(img: UploadFile = File(...)):
-#     original_image = Image.open(img.file)
-#     #original_image = original_image.filter(ImageFilter.BLUR)
-#     filtered_image = BytesIO()
-#     original_image.save(filtered_image, "JPEG")
-#     filtered_image.seek(0)
-#     return Response(content= filtered_image.getvalue(), media_type="image/jpeg")
-#     return StreamingResponse(filtered_image, media_type="image/jpeg")
-    
