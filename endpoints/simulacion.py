f44e657b288e4b15472d0c228f19def57f858a67
Guest <guest-ilcuuk@lab75.famaf.unc.edu.ar>
2022-10-27 12:14:42 -0300

    PYR-88 incompleto, seguir en casa

--- endpoints/simulacion.py
+++ endpoints/simulacion.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends, Form
+from .functions_jwt import *
 import engine
 
 
@@ -9,6 +10,16 @@ router = APIRouter()
 async def estado_juego():
     try:
         return engine.run_demo_game()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='error en la simulación')
+
+@router.post("/create_simulation")
+async def simulation(user_id: int = Depends(authenticated_user),
+                     id_robots: List[int] = Form(...)
+                    ):
+    try:
+        g = engine.Game([]).simulation()
     except Exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail='error en la simulación')
