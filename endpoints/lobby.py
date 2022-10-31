6910edc776ca3f7ebae78c73dab284c2b1623a67
LopezLeandro013 <leandro.lopez@mi.unc.edu.ar>
2022-10-31 20:00:47 -0300

    PYR-86 hotfix

--- endpoints/lobby.py
+++ endpoints/lobby.py
@@ -9,19 +9,20 @@ router = APIRouter()
 async def websocket_manager(websocket: WebSocket, partida_id: int):
     await lobby_manager.connect(websocket, partida_id)
     with db_session:
-        if Partida.get(partida_id=partida_id) is not None:
-            try:
-                msg = lobby_manager.last_msg[partida_id]
-                await lobby_manager.send_msg(partida_id, websocket, msg)
-            except Exception:
-                await lobby_manager.send_msg(partida_id, websocket, 'start')
-            try:
-                while True:
-                    data = await websocket.receive_text()
-                    await lobby_manager.broadcast(partida_id, data)
-            except WebSocketDisconnect:
-                lobby_manager.disconnect(websocket, partida_id)
-        else:
+        partida = Partida.get(partida_id=partida_id) is not None
+    if partida is True:
+        try:
+            msg = lobby_manager.last_msg[partida_id]
+            await lobby_manager.send_msg(partida_id, websocket, msg)
+        except Exception:
+            await lobby_manager.send_msg(partida_id, websocket, 'start')
+        try:
+            while True:
+                data = await websocket.receive_text()
+                await lobby_manager.broadcast(partida_id, data)
+        except WebSocketDisconnect:
             lobby_manager.disconnect(websocket, partida_id)
-            await lobby_manager.send_msg(partida_id, websocket, 'error')
-            await websocket.close()
+    else:
+        lobby_manager.disconnect(websocket, partida_id)
+        await lobby_manager.send_msg(partida_id, websocket, 'error')
+        await websocket.close()
