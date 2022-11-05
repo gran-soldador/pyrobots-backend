53986a66e1bc4ac1d759b5397a07db4061f471da
LopezLeandro013 <leandro.lopez@mi.unc.edu.ar>
2022-11-04 23:52:34 -0300

    refactorizacion websocket

--- websocket.py
+++ websocket.py
@@ -19,51 +19,39 @@ class LobbyManager:
     def disconnect(self, websocket: WebSocket, partida_id: int):
         self.room[partida_id].remove(websocket)
 
+    def create_msg(self, partida_id:int, msg:str):
+        if msg == 'error':
+            return {
+                "event": 'room not found',
+                "creador": None,
+                "contraseña": None,
+                "robot": None
+            }
+        with db_session:
+            msg = {
+                "event": msg,
+                "creador": Partida[partida_id].creador.nombre_usuario,
+                "contraseña": Partida[partida_id].password is not None,
+                "robot": [{"id": r.robot_id, "nombre": r.nombre,
+                           "usuario": r.usuario.nombre_usuario} for r in
+                          list(Partida[partida_id].participante)]
+            }
+        return msg
+
     async def broadcast(self, partida_id: int, msg: str):
         self.last_msg[partida_id] = msg
-        with db_session:
-            msg = (
-                {
-                    "event": msg,
-                    "creador": Partida[partida_id].creador.nombre_usuario,
-                    "contraseña": Partida[partida_id].password is not None,
-                    "robot": [{"id": r.robot_id, "nombre": r.nombre,
-                               "usuario": r.usuario.nombre_usuario} for r in
-                              list(Partida[partida_id].participante)]
-                }
-            )
         try:
             room = self.room[partida_id]
+            msg = self.create_msg(partida_id, msg)
             for connection in room:
-                if room != []:
-                    await connection.send_json(msg)
+                await connection.send_json(msg)
         except Exception:  # pragma: no cover
             pass
 
     async def send_msg(self, partida_id: int, websocket: WebSocket, msg: str):
-        with db_session:
-            try:
-                self.last_msg[partida_id] = msg
-                msg = (
-                    {
-                        "event": msg,
-                        "creador": Partida[partida_id].creador.nombre_usuario,
-                        "contraseña": Partida[partida_id].password is not None,
-                        "robot": [{"id": r.robot_id, "nombre": r.nombre,
-                                   "usuario": r.usuario.nombre_usuario}
-                                  for r in
-                                  list(Partida[partida_id].participante)]
-                    }
-                )
-            except Exception:
-                msg = (
-                    {
-                        "event": 'room not found',
-                        "creador": None,
-                        "contraseña": None,
-                        "robot": None
-                    }
-                )
+        if msg != 'error':
+            self.last_msg[partida_id] = msg
+        msg = self.create_msg(partida_id, msg)
         await websocket.send_json(msg)
 
 
