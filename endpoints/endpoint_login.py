a12f056dedff8685f09f346e1962f995c2531d34
Luigi Finetti <luigifinetti@mi.unc.edu.ar>
2022-11-15 10:47:38 -0300

    PYR-124 Traduce y refactoriza login

--- endpoints/endpoint_login.py
+++ endpoints/endpoint_login.py
@@ -7,25 +7,22 @@ from .validation import *
 
 auth_routes = APIRouter()
 
-# Login de usuario que genera token de autenticación
-
 
 @auth_routes.post("/login",
                   tags=['User Methods'],
-                  name='ingresar como usuario')
+                  name='Login')
 async def login(username: str = Form(...),
                 password: str = Form(...)):
     with db_session:
-        if not user_exist(username):
+        user = User.get(name=username)
+        if user is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail="User doesn't exist.")
-        elif not correct_login(username, password):
+        elif user.password != password:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Wrong Password.")
-        elif not Usuario.get(nombre_usuario=username).verificado:
+        elif not user.verified:
             detail = "User isn't verified yet. Please verify your account."
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail=detail)
-        else:
-            user_id = Usuario.get(nombre_usuario=username).user_id
-            return {'accessToken': gen_session_token({"user_id": user_id})}
+        return {'accessToken': gen_session_token({"user_id": user.id})}
