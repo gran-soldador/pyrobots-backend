38ae9d8b51a80c13ce63716eee510914239b3d86
Luigi Finetti <luigifinetti@mi.unc.edu.ar>
2022-11-15 11:12:10 -0300

    PYR-124 Traduce list_robots

--- endpoints/listar_robot.py
+++ endpoints/listar_robot.py
@@ -21,18 +21,19 @@ class RobotResult:
             tags=['Robot Methods'],
             name='List user robots',
             response_model=List[RobotResult])
-async def listar_robots(user_id: int = Depends(authenticated_user)):
+async def list_robots(user_id: int = Depends(authenticated_user)):
     with db_session:
-        lista = [RobotResult(id=r.id, name=r.name,
-                 avatar="http://localhost:9000/" + r.avatar,
-                 played=r.matches_num_played,
-                 won=r.matches_num_won,
-                 games=r.games_won,
-                 rounds=r.rounds_won,
-                 avg_rounds=r.rounds_won / r.games_won if
-                 r.games_won != 0 else 0)
-                 for r in User[user_id].robot]
-        if lista == []:
+        robots = [RobotResult(id=r.id, name=r.name,
+                              avatar="http://localhost:9000/" + r.avatar,
+                              played=r.matches_num_played,
+                              won=r.matches_num_won,
+                              games=r.games_won,
+                              rounds=r.rounds_won,
+                              avg_rounds=r.rounds_won / r.games_won if
+                              r.games_won != 0 else 0)
+                  for r in User[user_id].robot]
+        # TODO: Get static file server URL from env
+        if robots == []:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='No se encontraron robots')
-    return lista
+    return robots
