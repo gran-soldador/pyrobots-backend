430f93bcdbe73a9c12c3ddd1c06f636239681afb
Luigi Finetti <luigifinetti@mi.unc.edu.ar>
2022-11-18 15:53:12 -0300

    PYR-124 Traducir match_join

--- endpoints/unir_partida.py
+++ endpoints/unir_partida.py
@@ -9,38 +9,38 @@ router = APIRouter()
 @router.post('/match/join',
              tags=["Match Methods"],
              name="Join a match")
-async def unir_partida(user_id: int = Depends(authenticated_user),
-                       match_id: int = Form(...),
-                       password: str = Form(None),
-                       robot_id: int = Form(...)):
+async def match_join(user_id: int = Depends(authenticated_user),
+                     match_id: int = Form(...),
+                     password: str = Form(None),
+                     robot_id: int = Form(...)):
     with db_session:
-        partida = Partida.get_for_update(partida_id=match_id)
-        if partida is None:
+        match = Match.get_for_update(id=match_id)
+        if match is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='la partida no existe')
-        if partida.status != 'disponible':
+        if match.status != 'disponible':
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='partida no disponible')
-        if partida.password != password:
+        if match.password != password:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='contraseña incorrecta')
-        if user_id == partida.creador.user_id:
+        if user_id == match.owner.id:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='no puede unirse a su propia partida')
-        user = list(partida.participante)
-        user = list(filter(lambda r: r.usuario.user_id == user_id, user))
+        user = list(match.players)
+        user = list(filter(lambda r: r.user.id == user_id, user))
         if user != []:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='usuario ya unido')
-        robot = Robot.get(robot_id=robot_id)
+        robot = Robot.get(id=robot_id)
         if robot is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='el robot no existe')
-        if robot.usuario.user_id != user_id:
+        if robot.user.id != user_id:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='el robot no pertenece al usuario')
-        partida.participante.add(robot)
-        partida.flush()
-        if len(partida.participante) == partida.maxplayers:
-            partida.status = 'ocupada'
+        match.players.add(robot)
+        match.flush()
+        if len(match.players) == match.max_players:
+            match.status = 'ocupada'
     await lobby_manager.broadcast(match_id, 'join')
